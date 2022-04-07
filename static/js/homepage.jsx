154cf7a96ce63699053139c71290a6b06228483f
Caroline <carolinesitter24@gmail.com>
2022-04-07 18:28:42 -0500

    Completed Step 1

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,15 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+
+  <div>
+    <p>Welcome to Trading Cards!</p>
+    <a href = "/cards">Click here to view the trading cards</a>
+    <br></br>
+    <img src ="/static/img/balloonicorn.jpg"/>
+  </div>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
